6090724a8ae733b7b602e6c651e2012e58682a85
Waqar Ahmed <54082156+waqar-ahmed51@users.noreply.github.com>
2022-08-03 16:59:21 +0500

    mui-material, styled-components, mui-icon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import { Alert } from '@mui/material';
 import './App.css';
+import AccessibleIcon from '@mui/icons-material/Accessible';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>test</h1>
+      <Alert >sfasf</Alert>
+      <AccessibleIcon />
     </div>
   );
 }
